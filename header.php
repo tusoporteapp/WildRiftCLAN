4782913c9dab6f22045ce2ca83ae530e1ff2b5a5
tusoporteapp <84728004+tusoporteapp@users.noreply.github.com>
2021-05-29 17:53:27 -0500

    Add files via upload

---
+++ header.php
@@ -0,0 +1,36 @@
+
+<!-- bloc-0 -->
+<div class="bloc bloc-fill-screen l-bloc bloc-bg-texture texture-darken " id="bloc-0">
+	<div class="container fill-bloc-top-edge">
+		<div class="row">
+			<div class="col-12">
+				<div class="row">
+					<div class="col-12">
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="container">
+		<div class="row">
+			<div class="col">
+				<h1 class="text-center mg-clear hero-header-text text-fire-gradient">
+					WildRift<br>
+				</h1>
+				<h3 class="mg-md float-none text-center hero-subheader tc-black">
+					We are so glad you finally found us.
+				</h3>
+			</div>
+		</div>
+	</div>
+	<div class="container fill-bloc-bottom-edge">
+		<div class="row">
+			<div class="col-12">
+				<div class="text-center">
+					<a href="#" data-scroll-speed="1000" onclick="scrollToTarget('0',this)"><span class="fa fa-angle-down icon-md animated bounce animDelay08"></span></a>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<!-- bloc-0 END -->
---
+++ index.php
@@ -0,0 +1,47 @@
+<!doctype html>
+<html lang="es">
+<head>
+    <meta charset="utf-8">
+    <meta name="keywords" content="">
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover">
+	<meta name="robots" content="index, follow" />
+    <link rel="shortcut icon" type="image/png" href="<?php if (get_option('site_icon')){echo get_site_icon_url();}else{ echo get_template_directory_uri().'/favicon.png';}?>">
+    
+	
+    <title>Home</title>
+
+	<?php wp_head(); ?>
+
+    
+<!-- Analytics -->
+ 
+<!-- Analytics END -->
+    
+</head>
+<body>
+
+<!-- Preloader -->
+<div id="page-loading-blocs-notifaction" class="page-preloader"></div>
+<!-- Preloader END -->
+
+
+<!-- Main container -->
+<div class="page-container">
+    
+<?php get_header(); ?>
+
+<!-- ScrollToTop Button -->
+<a class="bloc-button btn btn-d scrollToTop" onclick="scrollToTarget('1',this)"><svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 32 32"><path class="scroll-to-top-btn-icon" d="M30,22.656l-14-13-14,13"/></svg></a>
+<!-- ScrollToTop Button END-->
+
+
+<?php get_footer(); ?>
+
+</div>
+<!-- Main container END -->
+    
+<?php wp_footer(); ?>
+
+</body>
+</html>
